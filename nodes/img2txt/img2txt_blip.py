101208b04037d92fb6a94e3b55f352b82e63a890
christian-byrne <abolkonsky.rem@gmail.com>
2024-03-28 17:55:20 -0700

    conditional image captioning

--- nodes/img2txt/img2txt_blip.py
+++ nodes/img2txt/img2txt_blip.py
@@ -3,13 +3,10 @@
 pyenv local 3.10.6"""
 
 import torch
-from PIL import Image
-
-from termcolor import colored
-from typing import Tuple
-
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from torchvision import transforms
+from termcolor import colored
+from typing import Tuple
 
 try:
     from ...utils.tensor_utils import TensorImgUtils
@@ -27,10 +24,12 @@ class Img2TxtBlipNode:
         return {
             "required": {
                 "input_image": ("IMAGE",),
+            },
+            "optional": {
                 "conditional_image_captioning": (
                     "STRING",
                     {
-                        "default": "a photograph of",
+                        # "default": "a photograph of",
                         "multiline": True,
                     },
                 ),
@@ -48,8 +47,6 @@ class Img2TxtBlipNode:
             "hidden": {
                 "unique_id": "UNIQUE_ID",
                 "extra_pnginfo": "EXTRA_PNGINFO",
-            },
-            "optional": {
                 "output_text": (
                     "STRING",
                     {
@@ -62,10 +59,7 @@ class Img2TxtBlipNode:
     RETURN_TYPES = ("STRING",)
     RETURN_NAMES = ("caption",)
     FUNCTION = "main"
-
-    # INPUT_IS_LIST = True
-    # OUTPUT_NODE = True
-    # OUTPUT_IS_LIST = (True,)
+    OUTPUT_NODE = True
 
     def main(
         self,
@@ -73,12 +67,10 @@ class Img2TxtBlipNode:
         conditional_image_captioning: str,
         exclude_terms: str,
         skip_special_tokens: bool,
-        output_text: str,
+        output_text: str = "",
         unique_id=None,
         extra_pnginfo=None,
     ) -> Tuple[str, ...]:
-        
-        print(colored(f"kwards dict: {locals()}", "green"))
         processor = BlipProcessor.from_pretrained(
             "Salesforce/blip-image-captioning-large"
         )
@@ -92,7 +84,7 @@ class Img2TxtBlipNode:
 
         if conditional_image_captioning == "":
             # unconditional image captioning
-            inputs = processor(raw_image, return_tensors="pt").to("cuda")
+            inputs = processor(raw_image, "an image of", return_tensors="pt").to("cuda")
         else:
             # conditional image captioning
             inputs = processor(
@@ -109,20 +101,17 @@ class Img2TxtBlipNode:
             for term in exclude_terms:
                 out_string = out_string.replace(term, "")
 
-        print(colored(f"{extra_pnginfo['workflow']}", "green"))
-        print(colored(f"unique_id: {unique_id}", "green"))
+        # if unique_id and extra_pnginfo and "workflow" in extra_pnginfo:
+        #     workflow = extra_pnginfo["workflow"]
+        #     node = [x for x in workflow["nodes"] if str(x["id"]) == unique_id[0]][0]
 
-        if unique_id and extra_pnginfo and "workflow" in extra_pnginfo:
-            workflow = extra_pnginfo["workflow"]
-            node = [x for x in workflow["nodes"] if str(x["id"]) == unique_id[0]][0]
-
-            # node = next(
-            #     (x for x in workflow["nodes"] if str(x["id"]) == unique_id[0]), None
-            # )
-            if node:
-                print(colored(f"node: {node['widgets_values']}", "green"))
-                node["widgets_values"] = [
-                    out_string
-                ]
+        #     # node = next(
+        #     #     (x for x in workflow["nodes"] if str(x["id"]) == unique_id[0]), None
+        #     # )
+        #     if node:
+        #         print(colored(f"node: {node['widgets_values']}", "green"))
+        #         node["widgets_values"] = [
+        #             out_string
+        #         ]
 
         return {"ui": {"text": out_string}, "result": (out_string,)}
